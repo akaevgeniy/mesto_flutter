db8b811c6c2ee666836e0755b9ffce376b51496e
evgeniy16 <akaevgeniy@yandex.ru>
2023-10-04 22:53:45 +0300

    Register auth

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mesto_flutter/data/api/auth.dart';
 import 'package:mesto_flutter/data/http/dio_client.dart';
 import 'package:mesto_flutter/data/models/auth_user.dart';
 import 'package:mesto_flutter/data/repository/user_repository.dart';
+import 'package:mesto_flutter/features/auth/bloc/auth_bloc.dart';
+import 'package:mesto_flutter/features/auth/repository/auth_repository.dart';
 import 'package:talker_flutter/talker_flutter.dart';
 
 final getIt = GetIt.instance;
@@ -14,4 +17,8 @@ Future<void> setupLocator() async {
   getIt.registerSingleton(talker);
   getIt.registerSingleton(AuthUser(email: ''));
   getIt.registerSingleton(UserRepository(getIt.get<AuthUser>()));
+  getIt.registerSingleton(AuthApi(dioClient: getIt<DioClient>()));
+  getIt.registerSingleton(AuthRepository(getIt.get<AuthApi>()));
+  getIt.registerSingleton(AuthBloc(getIt.get<AuthRepository>()));
+  getIt.registerSingleton(UserProfile(UserProfile.empty()));
 }
